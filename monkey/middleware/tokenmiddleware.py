51e79d11c2b09526da2e7b556340791c7b976ae5
2248022127 <2248022127@qq.com>
2017-11-29 10:23:36 +0800

    购物车API 0.5版  完成GET POST PUT DELETE基本请求，尚未前端对接

--- monkey/middleware/tokenmiddleware.py
+++ monkey/middleware/tokenmiddleware.py
@@ -1,14 +1,49 @@
-from django.utils.deprecation import MiddlewareMixin
+# from django.utils.deprecation import MiddlewareMixin
+#
+# class ToekenMD(MiddlewareMixin):
+#     def process_response(self,request,response):
+#         # print('>>>>', request.COOKIE)
+#         if request.method == 'OPTIONS':
+#             response['Access-Control-Allow-Methods'] = "POST"  # 允许跨域访问的 请求方式
+#             response['Access-Control-Allow-Origin']="http://localhost:8090"
+#             response['Access-Control-Allow-Headers'] = "Content-Type"  # 允许请求头 值为 请求头的key
+#             response['Access-Control-Allow-Credentials'] = "true"  #
+#         else:
+#             response['Access-Control-Allow-Origin'] = "http://localhost:8090"
+#             response['Access-Control-Allow-Credentials'] = "true"
+#         return response
 
+class MiddlewareMixin(object):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        super(MiddlewareMixin, self).__init__()
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
+
+
+
+from django.shortcuts import HttpResponse
+""" CORS 解决跨域请求 """
 class ToekenMD(MiddlewareMixin):
-    def process_response(self,request,response):
-        # print('>>>>', request.COOKIE)
+    """ CORS 解决跨域请求问题 """
+    def process_request(self, request):
+        """ 请求进来时候会执行的方法 """
+
         if request.method == 'OPTIONS':
-            response['Access-Control-Allow-Methods'] = "POST"  # 允许跨域访问的 请求方式
-            response['Access-Control-Allow-Origin']="http://127.0.0.1:8090"
-            response['Access-Control-Allow-Headers'] = "Content-Type"  # 允许请求头 值为 请求头的key
-            response['Access-Control-Allow-Credentials'] = "true"  #
-        else:
-            response['Access-Control-Allow-Origin'] = "http://127.0.0.1:8090"
-            response['Access-Control-Allow-Credentials'] = "true"
+            return HttpResponse()
+
+    def process_response(self, request, response):
+        """ 请求出去时执行方法 """
+        response['Access-Control-Allow-Origin'] = "http://localhost:8090"  #允许跨域访问的网站
+        response['Access-Control-Allow-Methods'] = "POST,PUT,PATCH,GET,DELETE"  # 允许跨域访问的 请求方式
+        response['Access-Control-Allow-Headers'] = "Content-Type,token"  # 允许请求头 值为 请求头的key
+        response['Access-Control-Allow-Credentials'] = "true"  #运行客户端携带证书式访问
         return response
--- monkey/views.py
+++ monkey/views.py
@@ -104,3 +104,89 @@ class CoursesView(APIView):
         #拿到所有课程
         response = HttpResponse(json.dumps(ser.data))
         return response
+
+
+
+
+""" Redis数据库 """
+import redis
+pool =redis.ConnectionPool(host='192.168.16.43', port=6379)
+r = redis.Redis(connection_pool=pool)
+
+"""价格与有课程效期表序列化"""
+class ShopPriceJson(serializers.ModelSerializer):  # 模板
+    valid_period = serializers.CharField(source='get_valid_period_display')
+    class Meta:
+        model = models.PricePolicy
+        fields = ['valid_period','id','price']
+
+""" 购物车API """
+class Shopping(APIView):
+    # obj = models.Course.objects.get(pk=1).price_policy #根据可能,或许关联的可能(多)
+    # obj = models.PricePolicy.objects.get(pk=2).content_type #根据 价格与有课程效期表 找关联课程
+    def get(self,request,*args,**kwargs):
+        """ 获取购物车信息 """
+
+        nid = 1 #用户id
+        info = {'code': 200, 'msg': '', 'content': {'course':{}}} #返回的数据
+
+        if not nid:
+            info = {'code': 401, 'msg': '尚未登陆', 'content': ''}  # 状态信息
+            return Response(info)
+
+        course_list = r.hkeys('price_user_%s'%nid)   #所有的课程对象
+        try:
+            for i in course_list:
+                i = str(i,encoding='utf-8')
+                course_obj = models.Course.objects.get(id=i)#课程对象
+                info['content']['course']['%s'%course_obj.pk] = {'name':course_obj.name,'default':i,'policy':ShopPriceJson(instance=course_obj.price_policy,many=True).data}
+        except:
+            info = {'code': 400, 'msg': '获取数据错误', 'content': {'course': {}}}  # 返回的数据
+        return Response(info)
+
+    def post(self,request,*args,**kwargs):
+        """ 增加购物车信息 """
+
+        info = {'code':200,'msg':'','content':''}   #状态信息
+        nid = '1'   #用户id
+        pid = '1'   #价格与课程表id
+        if not nid:
+            info = {'code': 401, 'msg': '尚未登陆', 'content': ''}  # 状态信息
+            return Response(info)
+        try:
+            obj = models.PricePolicy.objects.get(pk=pid) #课程对象
+            if r.hget('price_user_%s'%nid,str(obj.pk)):
+                info = {'code': 204, 'msg': '', 'content': '更新数据'}  # 状态信息
+            r.hset('price_user_%s'%nid,obj.pk,pid)  #增加数据格式：price_user_用户id；课程id；价格与课程表id
+        except:
+            info = {'code': 400, 'msg': '添加出错', 'content': ''}
+
+        return Response(info)
+
+    def delete(self,request,*args,**kwargs):
+        """ 删除购物车信息 """
+        info = {'code': 200, 'msg': '', 'content': '删除数据'}  # 状态信息
+        nid = '1' #用户id
+        pid = '3' #课程与课程表id
+        try:
+            obj = models.PricePolicy.objects.get(pk=pid)  # 课程对象
+            r.hdel('price_user_%s'%nid,obj.pk)  #删除数据
+        except:
+            info = {'code': 400, 'msg': '删除出错', 'content': ''}  # 状态信息
+        return Response(info)
+
+    def put(self,request,*args,**kwargs):
+        """ 更新购物车信息 """
+        nid = '1'   #用户id
+        pid = '1'   #价格与课程表id
+        info = {'code': 200, 'msg': '', 'content': '更新数据'}  # 状态信息
+        try:
+            obj = models.PricePolicy.objects.get(pk=pid)  # 课程对象
+            if r.hget('price_user_%s' % nid, str(obj.pk)):  #数据存在,则更新操作
+                r.hset('price_user_%s' % nid, obj.pk, pid)  # 增加数据格式：price_user_用户id；课程id；价格与课程表id
+            else:
+                info = {'code': 410, 'msg': '数据不存在', 'content': ''}
+        except:
+            info = {'code': 400, 'msg': '更新出错', 'content': ''}
+
+        return Response(info)
